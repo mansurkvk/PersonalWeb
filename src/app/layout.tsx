c576e17c0cf76f7a2fe0bac005ab62ed91d797e7
Muhammed Mansur KAVAK <58907427+mansurkvk@users.noreply.github.com>
2026-05-11 22:36:55 +0300

    Remove unused lab loop stylesheet import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Space_Grotesk } from "next/font/google";
 import "./globals.css";
-import "./lab-loop.css";
 import { SiteShell } from "@/components/site-shell";
 import { siteConfig } from "@/config/site";
 

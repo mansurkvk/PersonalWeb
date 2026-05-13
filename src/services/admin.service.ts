0caf62c4b8a54bcb58864ef6911ed4ac4858eeed
Muhammed Mansur KAVAK <58907427+mansurkvk@users.noreply.github.com>
2026-05-14 00:48:50 +0300

    Include contact messages in admin stats

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,17 +1,19 @@
 import { countBlogPosts } from "@/repositories/blog.repository";
 import { countBrokerMessages } from "@/repositories/broker.repository";
 import { countComments } from "@/repositories/comments.repository";
+import { countContactMessages } from "@/repositories/contact.repository";
 import { countTelemetryDevices } from "@/repositories/devices.repository";
 import { latestTelemetryReading } from "@/repositories/telemetry.repository";
 import { countProjects } from "@/repositories/projects.repository";
 import { countUsers } from "@/repositories/users.repository";
 
 export async function getAdminStats() {
-  const [userCount, blogCount, projectCount, commentCount, deviceCount, brokerMessageCount, latestTelemetry] = await Promise.all([
+  const [userCount, blogCount, projectCount, commentCount, contactMessageCount, deviceCount, brokerMessageCount, latestTelemetry] = await Promise.all([
     countUsers(),
     countBlogPosts(),
     countProjects(),
     countComments(),
+    countContactMessages(),
     countTelemetryDevices(),
     countBrokerMessages(),
     latestTelemetryReading()
@@ -22,6 +24,7 @@ export async function getAdminStats() {
     blogCount,
     projectCount,
     commentCount,
+    contactMessageCount,
     deviceCount,
     brokerMessageCount,
     latestTelemetry
